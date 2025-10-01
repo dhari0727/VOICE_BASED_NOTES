e02a80dbb99cf1f3768d866be49f81575bfad724
Thakkar Dharini <dharinithakkar4@gmail.com>
2025-10-01 11:36:48 +0000

    Fixed duplicated darkTheme, cleaned up main.dart, updated app theme and router

--- lib/screens/add_edit_note_screen.dart
+++ lib/screens/add_edit_note_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../models/voice_note.dart';
 import '../providers/voice_notes_provider.dart';
+import 'package:go_router/go_router.dart';
 import '../../providers/auth_provider.dart';
 
 class AddEditNoteScreen extends StatefulWidget {
